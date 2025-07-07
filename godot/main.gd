18f0d567014224d998f3e20e358e567a02e7b601
panicdragon <d.takayama.web@gmail.com>
2025-07-08 02:30:54 +0900

    feat: 🎸 add led python

--- godot/main.gd
+++ godot/main.gd
@@ -1,11 +1,54 @@
 extends Node2D
 
+const PORT = 9080
 
-# Called when the node enters the scene tree for the first time.
+var tcp_server := TCPServer.new()
+var peers: Dictionary = {}
+
+@onready var color_picker: ColorPicker = %ColorPicker
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if tcp_server.listen(PORT) != OK:
+		print("Unable to start server.")
+		set_process(false)
+	
+	color_picker.connect("color_changed", _on_color_changed)
+	print("🎨 ColorPicker ready.")
 
+func _process(_delta: float) -> void:
+	while tcp_server.is_connection_available():
+		var conn: StreamPeerTCP = tcp_server.take_connection()
+		if conn:
+			var peer: WebSocketPeer = WebSocketPeer.new()
+			if peer.accept_stream(conn) == OK:
+				var id := conn.get_connected_host() + ":" + str(conn.get_connected_port())
+				peers[id] = peer
+				print("🔌 client connection: %s" % id)
+			else:
+				print("⚠️ Connection Failure")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	for id in peers.keys():
+		var peer: WebSocketPeer = peers[id]
+		peer.poll()
+
+		while peer.get_available_packet_count() > 0:
+			var packet := peer.get_packet()
+			var msg := packet.get_string_from_utf8()
+			print("📨 reception: ", msg)
+
+func _exit_tree() -> void:
+	for id in peers:
+		peers[id].close()
+	tcp_server.stop()
+
+func _on_color_changed(new_color: Color) -> void:
+	var is_include_alpha=true
+	var color_hex := new_color.to_html(is_include_alpha)
+	print("🎨 Color changed to: ", color_hex)
+
+	var json := {"type": "color", "value": color_hex}
+	var packet := JSON.stringify(json).to_utf8_buffer()
+
+	for peer in peers.values():
+		if peer.get_ready_state() == WebSocketPeer.STATE_OPEN:
+			peer.put_packet(packet)
---
+++ rpi_ws281x_python/main.py
@@ -0,0 +1,76 @@
+import RPi.GPIO as GPIO
+import websocket
+import json
+from rpi_ws281x import PixelStrip, Color
+import time
+
+# IP and port of Godot side server
+SERVER_URI = "ws://<IP Address>:9080"
+
+# LED setting
+LED_COUNT = 144       # Number of LEDs
+LED_PIN = 18          # GPIO pins (18 supports PWM)
+LED_FREQ_HZ = 800000  # LED Signal Frequency
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+def hex_to_rgb(hex_str):
+    hex_str = hex_str.lstrip('#')
+    r = int(hex_str[0:2], 16)
+    g = int(hex_str[2:4], 16)
+    b = int(hex_str[4:6], 16)
+    return r, g, b
+
+def laser(strip, color, wait_ms=10):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 3000.0)
+        strip.setPixelColor(i, Color(0, 0, 0))
+        strip.show()
+
+def colorWipe(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+
+def on_message(ws, message):
+    if isinstance(message, bytes):
+        message = message.decode('utf-8')
+
+    try:
+        data = json.loads(message)
+        if data.get("type") == "color":
+            hex_color = data.get("value", "#000000")
+            r, g, b = hex_to_rgb(hex_color)
+            print("color received: R={} G={} B={}".format(r, g, b))
+            colorWipe(strip, Color(r, g, b))
+    except Exception as e:
+        print("error:", e)
+
+def on_open(ws):
+    print("WebSocket server connection succeeded")
+
+def on_error(ws, error):
+    print("error:", error)
+    GPIO.cleanup()
+
+def on_close(ws, code, reason):
+    print("end of connection")
+    if args.clear:
+        colorWipe(strip, Color(0, 0, 0), 10)
+    GPIO.cleanup()
+    print("GPIO cleaned up")
+
+laser(strip, Color(255, 255, 255))
+ws = websocket.WebSocketApp(SERVER_URI,
+                            on_open=on_open,
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
+ws.run_forever()
